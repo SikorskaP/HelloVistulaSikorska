d9e03913bfad01a1760af6a9d86aa3e4efc1e8bb
student <student@lab.pl.wsei>
2021-10-08 11:54:21 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldsikorska/HelloWorldSikorska.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldsikorska;
+
+public class HelloWorldSikorska {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Sikorska");
+    }
+}
